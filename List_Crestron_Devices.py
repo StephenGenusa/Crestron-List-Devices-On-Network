f07a9c8b5cd2a201c6e7ffb307489332bc5bb8f2
Stephen Genusa <github@genusa.com>
2017-07-25 18:46:28 -0500

    commenting added

--- List_Crestron_Devices.py
+++ List_Crestron_Devices.py
@@ -44,10 +44,15 @@ UDP_MSG = \
     ("\x00" * 252)
 
 for iface in netifaces.interfaces():
+    # if interface has ipv4 address
     if netifaces.AF_INET in netifaces.ifaddresses(iface):
+        # if addr attribute in interface dictionary
         if 'addr' in netifaces.ifaddresses(iface)[netifaces.AF_INET][0]:
+            # get the ipv4 address
             cur_ip = netifaces.ifaddresses(iface)[netifaces.AF_INET][0]['addr']
+            # tell user what we are testing
             print("Testing IP subnet", cur_ip)
+            # set the UDP test up
             sock = socket(AF_INET, SOCK_DGRAM)
             sock.bind((cur_ip, CIP_PORT))
             sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
@@ -55,20 +60,26 @@ for iface in netifaces.interfaces():
             sock.sendto(UDP_MSG, (BROADCAST_IP, CIP_PORT))
             sock.settimeout(1.0)
             try:
+                # we want a timeout exception to end the while True: loop based on timeout
                 while True:
+                    # get the response data
                     data, addr = sock.recvfrom(4096)
+                    # find the device hostname in the response buffer
                     search = re.findall ('\x00([a-zA-Z0-9-]{2,30})\x00', data[9:40])
                     if search:
                         dev_name = search[0]
+                        # get the ipv4 address taken from the sock.recvfrom function
                         dev_ip = addr[0]
-                        # add only new devices and skip our own packet
+                        # find if ver info is part of UDP packet
                         firmware_info = ""
-                        search = re.findall (r'\x00([\w].{10,80})\x00', data[265:350])
+                        search = re.findall ('\x00([\w].{10,80})\x00', data[265:350])
                         if search:
                             firmware_info = search[0]
+                        # add only new devices and skip our own packet
                         if dev_name not in crestron_devices and dev_name != "feed":
                             print((formatting + dev_name + " located at " + dev_ip).ljust(35) + \
                                   "\n" + (formatting * 2) + firmware_info)
+                            # save the device to a dictionary so we don't repeat it
                             crestron_devices[dev_name] = dev_ip
             except:
                 pass
